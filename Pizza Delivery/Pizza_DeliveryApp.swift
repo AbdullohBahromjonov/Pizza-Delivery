45628d14bf772653e79c647c259c0455b094bc13
Abdulloh <abdullohbahromjonv@gmail.com>
2024-01-13 21:29:16 +0500

    initial commit

---
+++ Pizza Delivery/Pizza_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_DeliveryApp.swift
+//  Pizza Delivery
+//
+//  Created by Abdulloh on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
