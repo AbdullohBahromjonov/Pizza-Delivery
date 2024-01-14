84e63e7d62c73b06fed70ff2450f7b63881d1f29
Abdulloh <abdullohbahromjonv@gmail.com>
2024-01-14 10:53:08 +0500

    updated networking part

--- Pizza Delivery/Menu Screen/View Model/Networking.swift
+++ Pizza Delivery/Menu Screen/View Model/Networking.swift
@@ -5,15 +5,41 @@
 //  Created by Abdulloh on 14/01/24.
 //
 
-import Foundation
+import SwiftUI
 
 class Networking: ObservableObject {
     @Published var foods: [Category]? = nil
     
-    func fetchData() async throws {
-        guard let url = URL(string: "https://my-json-server.typicode.com/AbdullohBahromjonov/FakeAPI/categories") else { fatalError("Wrong url") }
-        let (data, _) = try await URLSession.shared.data(from: url)
-        let decodedData = try JSONDecoder().decode([Category].self, from: data)
-        self.foods = decodedData
+    func fetchData() {
+        guard let url = URL(string: "https://my-json-server.typicode.com/AbdullohBahromjonov/FakeAPI/categories") else { fatalError("Missing URL") }
+        
+        let urlRequest = URLRequest(url: url)
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print("Request error: ", error)
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse else { return }
+
+            if response.statusCode == 200 {
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    do {
+                        let decodedData = try JSONDecoder().decode([Category].self, from: data)
+                        self.foods = decodedData
+                    } catch let error {
+                        print("Error decoding: ", error.localizedDescription)
+                    }
+                }
+            } else if (400...499).contains(response.statusCode) {
+                print("Client error")
+            } else if (500...599).contains(response.statusCode) {
+                print("Server error")
+            }
+        }
+
+        task.resume()
     }
 }
